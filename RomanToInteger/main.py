89844330469446377931d36d4649ebc318445e58
Donchuk Ivan <donchuk@protei.ru>
2022-09-02 19:08:37 +0300

    Third task

--- RomanToInteger/main.py
+++ RomanToInteger/main.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/roman-to-integer/
+# https://leetcode.com/problems/integer-to-roman/
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -18,8 +19,26 @@ class Solution:
             i += 1
         return sum(n)
 
+    def intToRoman(self, num: int) -> str:
+        roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        n = []
+        s = []
+        while num > 0:
+            n.append(num % 10)
+            num //= 10
+        n.reverse()
+        print(n)
+        for i in range(len(n) - 1):
+            for k, v in roman_numbers.items():
+                if v == n[i]:
+                    s.append(roman_numbers[k])
+        return str(s).replace(', ', '')
+
 
 if __name__ == '__main__':
-    print(Solution().romanToInt('MCMXCIV'))
-    print(Solution().romanToInt('LVIII'))
-    print(Solution().romanToInt('III'))
+    # print(Solution().romanToInt('MCMXCIV'))
+    # print(Solution().romanToInt('LVIII'))
+    # print(Solution().romanToInt('III'))
+    print(Solution().intToRoman(1994))
+    print(Solution().intToRoman(58))
+    print(Solution().intToRoman(3))
